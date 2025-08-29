875c055bb7d4bf01af7b647cffbf4f04aa6e0893
adal4ik <adilet.adilek@gmail.com>
2025-08-29 14:06:01 +0500

    feat: add service repo handler a

--- internal/app/service/service.go
+++ internal/app/service/service.go
@@ -3,16 +3,25 @@ package service
 import (
 	"sync"
 
+	"rsslibrary/internal/app/repository"
 	"rsslibrary/internal/app/service/workerpool"
 )
 
-type Service interface {
+type ServiceInterface interface {
 	Fetch()
 }
 
-type RequestServer struct{}
+type Service struct {
+	repository *repository.Repository
+}
 
-func (this *RequestServer) Fetch() {
+func NewService(repo *repository.Repository) *Service {
+	return &Service{
+		repository: repo,
+	}
+}
+
+func (this *Service) Fetch() {
 	bufferSize := 1000
 	var wg sync.WaitGroup
 	dispatcher := workerpool.NewDispatcher(bufferSize, &wg, 500)
--- internal/app/start.go
+++ internal/app/start.go
@@ -1,15 +1,21 @@
 package app
 
 import (
+	"context"
 	"fmt"
 	"io"
+	"log"
 	"net"
 	"strings"
 
 	"rsslibrary/internal/app/handlers"
+	"rsslibrary/internal/app/repository"
+	"rsslibrary/internal/app/service"
+	"rsslibrary/internal/config"
+	"rsslibrary/pkg/loadenv"
 )
 
-func RunApp() {
+func RunApp(ctx context.Context) {
 	listener, err := net.Listen("tcp", "localhost:8080")
 	if err != nil {
 		fmt.Println(err.Error())
@@ -17,18 +23,28 @@ func RunApp() {
 	}
 	defer listener.Close()
 	fmt.Println("Server is running on port 8080")
-
+	loadenv.LoadEnv("./.env")
+	cfg := config.Load()
+	db, err := repository.ConnectDB(ctx, cfg.Database)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	log.Println("Connected to DB:", db.Stats())
+	repo := repository.NewRepository(db)
+	service := service.NewService(repo)
+	handler := handlers.NewRequestHandler(service)
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println(err.Error())
 			continue
 		}
-		go handleClient(conn)
+		go handleClient(conn, ctx, handler)
 	}
 }
 
-func handleClient(conn net.Conn) {
+func handleClient(conn net.Conn, ctx context.Context, handler *handlers.RequestHandler) {
 	defer conn.Close()
 
 	// Parsing client request
@@ -48,7 +64,4 @@ func handleClient(conn net.Conn) {
 	fmt.Println(req.String())
 	// Processing the request
 
-	if req.String() == "fetch" {
-		handlers.Fetch(ctx)
-	}
 }
