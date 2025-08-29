e929540b316fd372e7e1fe252ac751fb51cae639
adal4ik <adilet.adilek@gmail.com>
2025-08-29 13:26:57 +0500

    feat(repo): add get all feeds method

--- internal/app/repository/feeds.go
+++ internal/app/repository/feeds.go
@@ -1,6 +1,9 @@
 package repository
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+)
 
 type FeedsRepositoryInterface interface {
 	// Define methods for FeedsRepositoryInterface
@@ -12,4 +15,27 @@ type FeedsRepository struct {
 
 func NewFeedsRepository(db *sql.DB) *FeedsRepository {
 	return &FeedsRepository{db: db}
+}
+
+func (r *FeedsRepository) InsertFeed(ctx context.Context) error {
+	// Implement the method to insert a feed into the database
+	return nil
+}
+
+func (r *FeedsRepository) GetAllFeeds(ctx context.Context) ([]string, error) {
+	query := "SELECT url FROM feeds ORDER BY updated_at ASC"
+	rows, err := r.db.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var feeds []string
+	for rows.Next() {
+		var url string
+		if err := rows.Scan(&url); err != nil {
+			return nil, err
+		}
+		feeds = append(feeds, url)
+	}
+	return feeds, nil
 }
